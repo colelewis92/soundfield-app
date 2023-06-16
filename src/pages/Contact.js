f3aba7d5d3cdff58d63407d6d3ddf83aeb1d5699
Cole Lewis <cole@codebycole.com>
2023-06-16 16:36:46 -0400

    corrected compiler warnings in Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,21 +1,8 @@
-
-import React, { useRef } from 'react'
 import { EnvelopeIcon, DevicePhoneMobileIcon } from '@heroicons/react/24/outline'
 
 
 
 const Contact = () => {
-
-  const nameRef = useRef()
-  const emailRef = useRef()
-  const messageRef = useRef()
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    /* 
-    Do something here !
-    */
-  }
 
   return (
     <div className="relative mx-auto w-full max-w-7xl text-white">
@@ -52,7 +39,7 @@ const Contact = () => {
             {/* ::Socials */}
             <div className="flex items-center">
               {/* :FACEBOOK */}
-              <a href="https://www.facebook.com/soundfield1/" target='_blank' className="m-1.5 w-8 h-8 inline-flex justify-center items-center shadow-sm rounded-full bg-[#4267B2] text-white filter hover:brightness-125" style={{ backgroundColor: "#4267B2" }}>
+              <a href="https://www.facebook.com/soundfield1/" target='_blank' rel='noreferrer' className="m-1.5 w-8 h-8 inline-flex justify-center items-center shadow-sm rounded-full bg-[#4267B2] text-white filter hover:brightness-125" style={{ backgroundColor: "#4267B2" }}>
                 {/* ::facebook svg */}
                 <svg className="w-5 h-5 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                   <path d="M16.403,9H14V7c0-1.032,0.084-1.682,1.563-1.682h0.868c0.552,0,1-0.448,1-1V3.064c0-0.523-0.401-0.97-0.923-1.005C15.904,2.018,15.299,1.999,14.693,2C11.98,2,10,3.657,10,6.699V9H8c-0.552,0-1,0.448-1,1v2c0,0.552,0.448,1,1,1l2-0.001V21c0,0.552,0.448,1,1,1h2c0.552,0,1-0.448,1-1v-8.003l2.174-0.001c0.508,0,0.935-0.381,0.993-0.886l0.229-1.996C17.465,9.521,17.001,9,16.403,9z"/>
